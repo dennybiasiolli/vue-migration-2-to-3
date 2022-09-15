7834ce89c619e43021d5d28ec68e3a9330eec997
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2022-09-15 15:21:10 +0200

    tests: fixing jest spy on component method with a temporary workaround

--- tests/unit/views/CounterLocal.spec.js
+++ tests/unit/views/CounterLocal.spec.js
@@ -9,7 +9,8 @@ describe('CounterLocal.vue', () => {
 
   it('should call increment after button click', () => {
     const wrapper = shallowMount(CounterLocal)
-    const spyIncrement = jest.spyOn(wrapper.vm, 'increment')
+    // FIXME: spying on `wrapper.vm._setupState` methods is a temporary workaround
+    const spyIncrement = jest.spyOn(wrapper.vm._setupState, 'increment')
     wrapper.find('button').trigger('click')
     expect(spyIncrement).toHaveBeenCalled()
   })
