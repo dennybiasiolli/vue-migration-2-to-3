738da1f97e8866c785ae01043fad077965345d83
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2022-09-16 09:33:51 +0200

    detaching jest configuration from vue-cli-service [broken tests]

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,35 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest'
+  // preset: '@vue/cli-plugin-unit-jest'
+
+  testEnvironment: 'jsdom',
+  moduleFileExtensions: [
+    'js',
+    'json',
+    'vue'
+  ],
+  transform: {
+    '^.+\\.vue$': '@vue/vue2-jest',
+    // '.+\\.(css|styl|less|sass|scss|jpg|jpeg|png|svg|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|avif)$':
+    // require.resolve('jest-transform-stub'),
+    '^.+\\.jsx?$': require.resolve('babel-jest')
+  },
+  transformIgnorePatterns: ['/node_modules/'],
+  // support the same @ -> src alias mapping in source code
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  },
+  // serializer for snapshots
+  snapshotSerializers: [
+    'jest-serializer-vue'
+  ],
+  testMatch: [
+    '**/tests/unit/**/*.spec.[jt]s?(x)',
+    '**/__tests__/*.[jt]s?(x)'
+  ],
+  // https://github.com/facebook/jest/issues/6766
+  testURL: 'http://localhost/',
+  watchPlugins: [
+    require.resolve('jest-watch-typeahead/filename'),
+    require.resolve('jest-watch-typeahead/testname')
+  ]
 }
