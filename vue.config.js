c890d25bae11fb057a397a5531082ddbbfec5363
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2022-09-27 16:08:56 +0200

    removing @vue/compat dependencies and configuration

--- vue.config.js
+++ vue.config.js
@@ -2,21 +2,4 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
-  chainWebpack: (config) => {
-    config.resolve.alias.set('vue', '@vue/compat')
-
-    config.module
-      .rule('vue')
-      .use('vue-loader')
-      .tap((options) => {
-        return {
-          ...options,
-          compilerOptions: {
-            compatConfig: {
-              MODE: 3
-            }
-          }
-        }
-      })
-  }
 })
